d9f462f85a42d869127e9799b1b98643406c854b
Raynaud <lraynaud@cslab20.cs.skidmore.edu>
2014-09-17 19:37:01 -0400

    third lab

--- Baseball Calc/src/ForceCalc.java
+++ Baseball Calc/src/ForceCalc.java
@@ -7,38 +7,41 @@
  *
  */
 
-
+import java.util.Scanner;
 public class ForceCalc {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-			
-		// Method for 500 foot field//
-		double angle = 0.506145483; // this is the average exit angle (in radians) of a baseball from http://www.laserpablo.com/baseball/Kagan/HomerAngle.pdf//
-		double gravity = 9.8; //this is the gravitation constant in meters per second^2//
-		double d = 500; // this input will be in feet// 
-		double con = d*0.3048; // this converts the users input in feet to meters//
-		double v = (Math.sqrt((con)*(gravity))/(Math.sin(angle)*2)); // this calculates the velocity required for achieving the max trajectory (d)//
-		double fincon = (v*0.000621371)*(3600); // converts velocity to miles per hour from meters per second//	
-			System.out.println("You have to hit the ball  " + fincon  );
-			System.out.println("miles per hour to hit the ball");
-			System.out.println(d + "feet");
-			
-			threehundred(args);
+		
+		//create scanner called "console"//
+		Scanner console = new Scanner(System.in);
+		
+		//promte user to input length of baseball they wish to clear//
+		System.out.println("Length of baseball field in feet?");
+		
+		// user inputs desired length//
+		double d = console.nextDouble(); 
+		
+		// main method prints out velocity required to hit ball out of the specified field//
+		 System.out.println("You have to hit the ball  "+ calculatevelocity(d)); 
+		 System.out.println("miles per hour to hit the ball"); 
+		 System.out.println(d + "  feet"); 
+		
+	
 	}
-	public static void threehundred(String[] args) {
-		// Method for 500 foot field//
+		
+	public static double calculatevelocity(double d)	{
+		
+		
 		double angle = 0.506145483; // this is the average exit angle (in radians) of a baseball from http://www.laserpablo.com/baseball/Kagan/HomerAngle.pdf//
 		double gravity = 9.8; //this is the gravitation constant in meters per second^2//
-		double d = 300; // this input will be in feet// 
 		double con = d*0.3048; // this converts the users input in feet to meters//
 		double v = (Math.sqrt((con)*(gravity))/(Math.sin(angle)*2)); // this calculates the velocity required for achieving the max trajectory (d)//
-		double fincon = (v*0.000621371)*(3600); // converts velocity to miles per hour from meters per second//	
-					System.out.println("You have to hit the ball  " + fincon  );
-					System.out.println("miles per hour to hit the ball");
-					System.out.println(d + "feet");
+		return (v*0.000621371)*(3600); // converts velocity to miles per hour from meters per second and then returns this info to the main method//	
+		
+		
+
 	}
 }
