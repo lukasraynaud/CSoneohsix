a623c777f0619268f2a1b531c2c7f3d8266606c7
Raynaud <lraynaud@cslab18.cs.skidmore.edu>
2014-10-15 19:48:05 -0400

    array testing

--- Reading and Writing Files/src/TextFiles.java
+++ Reading and Writing Files/src/TextFiles.java
@@ -1,12 +1,13 @@
-/**
+/** Program to Calculate Account Balances from Text File
  * 
  */
 
 /**
  * @author lraynaud
+
  *
  */
-
+// import appropriate utilities // 
 import java.io.*;
 import java.util.*;
 
@@ -14,49 +15,91 @@ public class TextFiles {
 
 	/**
 	 * @param args
+	 * Main Class
 	 */
+	
+	// Main Method Establishes Balance Variables, Scans File, and Outputs Results to New File//
 	public static void main(String[] args) throws FileNotFoundException {
 
-		double checkingAccount = 0;
-		double savingsAccount = 0;
-		double creditCardAccount = 0;
+		
+		// set variables for account balances starting at 0//
+		String [] dates = new String[113];
+		String [] accounts = new String [113];
+		String [] transactions = new String [113];
+		double [] amounts = new double [113];
+		
+		
+		// create new scanner for reading file from src folder //
+		Scanner fileReader = new Scanner(new File("src/input.txt"));
+		
+		// create new printstream for writing output file to src folder //
+		PrintStream output = new PrintStream(new File("src/output.txt"));
 
 		
-		Scanner fileReader = new Scanner(new File("src/input.txt"));
-		PrintStream output = new PrintStream(new File("src/output.txt"));
+		// create while loop to read through entire text file, as long as there are additional tokens //
 		
+		int i = 0;
 		while (fileReader.hasNext()) {
-			fileReader.next();
-			String type = fileReader.next();
-			if (type.equals("Credit")) {
-				type += fileReader.next();
-			}
-			String action = fileReader.next();
-			if (action.equals("Opening")) {
-				action += fileReader.next();
-			}
-			double dollar = fileReader.nextDouble();
-			if (action.equals("Withdrawal")) {
-				dollar *= -1;
-			}
-			if(type.equals("Checking")){
-				checkingAccount += dollar;
-			}
-			else if(type.equals("Savings")){
-				savingsAccount += dollar;
-			}
-			else{
-				creditCardAccount += dollar;
-			}
 			
+		dates[i] = fileReader.next();
+		
+		accounts[i] = fileReader.next();
+		if (accounts [i].equals("Credit")) {
+			accounts[i] = accounts[i] + " " + fileReader.next();
 		}
-	output.println("Checking Balance =  $" + checkingAccount);
-	output.println("Savings Balance =  $" + savingsAccount);
-	output.println("Credit Card Balance =  $" + creditCardAccount);
-	output.println("You're not poor!");
+		
+		transactions[i] = fileReader.next();
+		if (transactions[i].equals("Opening")) {
+			transactions[i] = transactions[i] + " " + fileReader.next();
+			
+		
+		}
+	amounts[i] = fileReader.nextDouble();
 	
-	output.close();
+	i ++;
+	
+		}
+		fileReader.close();
+		
+		
+		double checking = 0;
+		double savings = 0;
+		double credit = 0;
+		
+		
+		for (i = 0; i < dates.length; i++){
+			if (accounts[i].equals("Checking")) {
+				if (transactions[i].equals("Withdrawal")) {
+					checking -= amounts[i];
+				} else { 
+					checking += amounts[i];
+					
+				}
+			} else if ( (accounts[i].equals("Savings"))){
+				if (transactions[i].equals("Withdrawal")){
+					savings -= amounts[i];
+				} else { savings +=amounts[i];
+				
+				}
+			} else {
+				if ( (transactions[i].equals("Withdrawal"))){
+					credit -= amounts[i];
+				} else {
+					credit += amounts[i];
+					
+				}
+			}
+				System.out.println("Checking balance $" + checking);
+				System.out.println("Savings balance $" + savings);
+				System.out.println("Credit balance $" + credit);
+				
 	
 	
+			
+		
+
+		}
 	}
+	
 }
+
